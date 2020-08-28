7c2516cdc3a59dd9b13a3c338c8a4e376100a462
Erik Norfelt <enorfelt@outlook.com>
2020-08-28 22:42:00 +0200

    wc calculations with spec

--- pressure-to-water-column.js
+++ pressure-to-water-column.js
@@ -1,15 +1,57 @@
 module.exports = function (RED) {
+
+  const GRAVITY =  9.807;
+  const VOLT = 4.096;
+
+  function getDescendantProp(obj, desc) {
+    var arr = desc.split(".");
+    while(arr.length && (obj = obj[arr.shift()]));
+    return obj;
+  };
+
   function PressureToWaterColumn(config) {
     RED.nodes.createNode(this, config);
+    
+    this.valueProperty = config.valueProperty;
+    this.voltageProperty = config.voltageProperty;
+    this.pressureLow = config.pressureLow || 0.0;
+    this.pressureHigh = config.pressureHigh || 6.0;
+    this.voltLow = config.voltLow || 0.0;
+    this.voltHigh = config.voltHigh || 5.0;
+    this.sensorHight = config.sensorHight || 0;
+
+
     var node = this;
+    var coefficientA = (node.pressureHigh - node.pressureLow) / (node.voltHigh - node.voltLow);
+    var coefficientB = coefficientA * node.voltLow - node.pressureLow;
+
     node.on("input", function (msg, send, done) {
-      msg.payload = msg.payload.toLowerCase();
-      send = send || function() { node.send.apply(node,arguments) };
+      var currentValue = getDescendantProp(msg, node.valueProperty);
+      var currentVoltage = getDescendantProp(msg, node.voltageProperty);    
+      
+      
+      if (!currentVoltage) {
+        if (!currentValue) {
+          if (done) {
+            done("Neither voltage or value is supplied");
+            return;
+          }
+        }
+
+        currentVoltage = currentValue * VOLT / 32767;
+      }
+
+      var currentPressure = coefficientA * currentVoltage + coefficientB;
+      var waterLevel = currentPressure / GRAVITY;
+      waterLevel = waterLevel * 100 + node.sensorHight;
+
+      msg.payload = waterLevel
+      send = send || function() { node.send.apply(node, arguments) };
       send(msg);
 
       if (done)
         done();
     });
-  }
+  };
   RED.nodes.registerType("pressure-to-water-column", PressureToWaterColumn);
 };
--- test/pressure-to-water-column_spec.js
+++ test/pressure-to-water-column_spec.js
@@ -1,5 +1,6 @@
+var should = require("should");
 var helper = require("node-red-node-test-helper");
-var lowerNode = require("../pressure-to-water-column.js");
+var pressureToWc = require("../pressure-to-water-column.js");
  
 helper.init(require.resolve('node-red'));
  
@@ -16,26 +17,84 @@ describe('pressure-to-water-column Node', function () {
  
   it('should be loaded', function (done) {
     var flow = [{ id: "n1", type: "pressure-to-water-column", name: "pressure-to-water-column" }];
-    helper.load(lowerNode, flow, function () {
+    helper.load(pressureToWc, flow, function () {
       var n1 = helper.getNode("n1");
       n1.should.have.property('name', 'pressure-to-water-column');
       done();
     });
   });
  
-  it('should make payload lower case', function (done) {
+  it('should calculate water column', function (done) {
     var flow = [
-      { id: "n1", type: "pressure-to-water-column", name: "pressure-to-water-column",wires:[["n2"]] },
+      { id: "n1",
+        type: "pressure-to-water-column",
+        name: "pressure-to-water-column",
+        valueProperty: "payload.value",
+        voltageProperty: "payload.voltage",
+        wires:[["n2"]] 
+      },
       { id: "n2", type: "helper" }
     ];
-    helper.load(lowerNode, flow, function () {
+    helper.load(pressureToWc, flow, function () {
       var n2 = helper.getNode("n2");
       var n1 = helper.getNode("n1");
       n2.on("input", function (msg) {
-        msg.should.have.property('payload', 'uppercase');
+        msg.should.have.property('payload', 61.180789232181084);
         done();
       });
-      n1.receive({ payload: "UpperCase" });
+      n1.receive({ payload: { value: 32767, voltage: 5.0 } });
+    });
+  });
+
+  it('should calculate water column with only value supplied', function (done) {
+    var flow = [
+      { id: "n1",
+        type: "pressure-to-water-column",
+        name: "pressure-to-water-column",
+        valueProperty: "payload.value",
+        voltageProperty: "payload.voltage",
+        wires:[["n2"]] 
+      },
+      { id: "n2", type: "helper" }
+    ];
+    helper.load(pressureToWc, flow, function () {
+      var n2 = helper.getNode("n2");
+      var n1 = helper.getNode("n1");
+
+      
+      n2.on("input", function (msg) {
+        msg.should.have.property('payload', 50.11930253900274);
+        done();
+      });
+      n1.receive({ payload: { value: 32767 } });
+    });
+  });
+
+  it('should call done with error when no payload', function (done) {
+    var flow = [
+      { id: "n1",
+        type: "pressure-to-water-column",
+        name: "pressure-to-water-column",
+        valueProperty: "payload.value",
+        voltageProperty: "payload.voltage",
+        wires:[["n2"]] 
+      },
+      { id: "n2", type: "helper" }
+    ];
+    helper.load(pressureToWc, flow, function () {
+      var n2 = helper.getNode("n2");
+      var n1 = helper.getNode("n1");
+
+      
+      n2.on("input", function (msg) {
+        done(new Error("No message should have been sent"));
+      });
+      n1.error = function(message) {
+        message.should.equal('Neither voltage or value is supplied');
+        done();
+      }
+      n1.emit("input", { payload: { } });
+      
     });
   });
 });
